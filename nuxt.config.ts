44afe4be4b8e958146ad995352f31f193991e6c3
Chris1320 <me@chrisfromnowhere.me>
2025-07-05 16:13:10 +0800

    build: use nuxt config to deploy
    Signed-off-by: Chris1320 <me@chrisfromnowhere.me>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,13 +4,18 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
     compatibilityDate: "2025-05-15",
     devtools: {
-      enabled: true,
-
-      timeline: {
         enabled: true,
-      },
+        timeline: { enabled: true },
     },
     css: ["~/assets/css/main.css"],
     modules: ["@nuxt/eslint", "@nuxt/test-utils"],
     vite: { plugins: [tailwindcss()] },
+    nitro: {
+        preset: "cloudflare_module",
+        cloudflare: {
+            wrangler: { name: "personal-website" },
+            deployConfig: true,
+            nodeCompat: true,
+        },
+    },
 });
