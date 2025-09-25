521409b103820286d6139c60f184288d75518941
Chris1320 <me@chrisfromnowhere.me>
2025-09-26 02:37:07 +0800

    feat: implement AppError component and enhance loading state in projects pages
    Signed-off-by: Chris1320 <me@chrisfromnowhere.me>

---
+++ components/AppError.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+const props = defineProps<{
+    error: string | null;
+}>();
+</script>
+
+<template>
+    <div class="flex flex-col justify-center items-center min-h-dvh">
+        <Icon name="lucide:circle-alert" size="128" class="text-red-200 mb-4" />
+        <h1 class="text-4xl font-bold mb-4">Error loading projects</h1>
+        <p class="text-lg text-center">{{ props.error }}</p>
+    </div>
+</template>
--- components/LandingPage/LandingPageProjects.vue
+++ components/LandingPage/LandingPageProjects.vue
@@ -1,21 +1,19 @@
 <script setup lang="ts">
-import { ref, onMounted } from "vue";
 import type { Project } from "~/utils/models/project";
 
-const projects = ref<Project[]>([]);
-
-onMounted(async () => {
-    const response = await fetch("/data/projects.json");
-    projects.value = await response.json();
-
-    console.log(`There are ${projects.value.length} projects.`);
-});
+const { data: projects, pending, error } = await useFetch<Project[]>("/data/projects.json", { server: false });
+console.log(`There are ${projects.value?.length} projects.`);
 </script>
 
 <template>
     <div class="container min-h-screen mx-auto p-4">
         <h1 class="text-4xl font-bold mb-4">Projects</h1>
-        <div v-for="project in projects" :key="project.id">
+        <div v-if="pending" class="mb-6 flex flex-row justify-center items-center">
+            <span class="loading loading-circle loading-md" />
+            <p class="pl-5 text-md">Loading projects...</p>
+        </div>
+        <div v-else-if="error"><AppError :error="error.message" /></div>
+        <div v-for="project in projects" v-else :key="project.id">
             <ProjectsProjectQuickGlance :project="project" />
         </div>
     </div>
