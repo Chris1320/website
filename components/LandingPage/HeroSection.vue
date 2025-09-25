c2f79f0953d1e88e8b63f13606018f6732b667b7
Chris1320 <me@chrisfromnowhere.me>
2025-09-25 09:55:10 +0800

    feat: add `LandingPageHeroSection` component and refactor index page
    Signed-off-by: Chris1320 <me@chrisfromnowhere.me>

---
+++ components/LandingPage/HeroSection.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="hero bg-base-200 min-h-screen">
+        <div class="hero-content text-center">
+            <div class="max-w-md">
+                <h1 class="text-5xl font-bold">Hi! 👋</h1>
+                <p class="pt-8">
+                    I&apos;m currently building this site. There&apos;s nothing to see here yet. You should check back
+                    later, though!
+                </p>
+                <p class="pb-7">In the meantime, you can check out my profiles on other platforms:</p>
+                <div>
+                    <NuxtLink to="https://github.com/Chris1320" target="_blank" class="m-4">
+                        <button class="btn btn-primary">GitHub</button>
+                    </NuxtLink>
+                    <NuxtLink to="https://www.credly.com/users/cfn" target="_blank" class="m-4">
+                        <button class="btn btn-secondary">Credly</button>
+                    </NuxtLink>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
