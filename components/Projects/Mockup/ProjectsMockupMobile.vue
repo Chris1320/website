a8e357d012b2c969391edcd1562ac52423bc9022
Chris1320 <me@chrisfromnowhere.me>
2025-09-25 19:49:10 +0800

    fix: mobile mockup's corners are too rounded
    Signed-off-by: Chris1320 <me@chrisfromnowhere.me>

--- components/Projects/Mockup/ProjectsMockupMobile.vue
+++ components/Projects/Mockup/ProjectsMockupMobile.vue
@@ -16,34 +16,36 @@ function nextImage() {
 </script>
 
 <template>
-    <div class="mockup-phone max-w-75 m-0">
-        <div class="mockup-phone-camera" />
-        <div class="mockup-phone-display">
-            <div v-if="data.images.length > 0" class="carousel w-full overflow-hidden">
-                <div
-                    v-for="(image, index) in props.data.images"
-                    v-show="carouselImageIndex === index"
-                    :id="`slide${index + 1}`"
-                    :key="index"
-                    class="carousel-item relative w-full"
-                >
-                    <img :src="image.url" class="w-full" :alt="image.description" />
-                    <!-- FIXME: Buttons readjust when images are longer than usual. -->
-                    <div
-                        v-show="data.images.length > 1"
-                        class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between"
-                    >
-                        <button class="btn btn-circle" @click="prevImage()">
-                            <Icon name="mdi:chevron-left" size="24" />
-                        </button>
-                        <button class="btn btn-circle" @click="nextImage()">
-                            <Icon name="mdi:chevron-right" size="24" />
-                        </button>
+    <div class="transform scale-50 origin-top m-auto">
+        <div class="mockup-phone">
+            <div class="mockup-phone-camera" />
+            <div class="mockup-phone-display">
+                <div class="w-100 h-auto">
+                    <div v-if="data.images.length > 0" class="carousel w-full overflow-hidden">
+                        <div
+                            v-for="(image, index) in props.data.images"
+                            v-show="carouselImageIndex === index"
+                            :id="`slide${index + 1}`"
+                            :key="index"
+                            class="carousel-item relative w-full"
+                        >
+                            <img :src="image.url" class="w-full" :alt="image.description" />
+                            <!-- FIXME: Buttons readjust when images are longer than usual. -->
+                            <div
+                                v-show="data.images.length > 1"
+                                class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between"
+                            >
+                                <button class="btn btn-circle" @click="prevImage()">
+                                    <Icon name="mdi:chevron-left" size="24" />
+                                </button>
+                                <button class="btn btn-circle" @click="nextImage()">
+                                    <Icon name="mdi:chevron-right" size="24" />
+                                </button>
+                            </div>
+                        </div>
                     </div>
+                    <div v-else class="grid place-content-center text-gray-500 h-full">No image provided</div>
                 </div>
-            </div>
-            <div v-if="data.images.length === 0" class="grid place-content-center text-gray-500 h-80">
-                No image provided
             </div>
         </div>
     </div>
