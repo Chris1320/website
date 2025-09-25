3dbd715d720e98ed876c4cdc9fa35a71958f1ce8
Chris1320 <me@chrisfromnowhere.me>
2025-09-25 09:43:26 +0800

    refactor: move footer to a reusable component
    Signed-off-by: Chris1320 <me@chrisfromnowhere.me>

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,20 +13,6 @@ useHead({
     <div>
         <NuxtRouteAnnouncer />
         <slot />
-        <footer class="footer sm:footer-horizontal bg-neutral text-neutral-content p-10">
-            <aside>
-                <p>&copy; 2025 Chris1320</p>
-                <p>Made with 💖 using Nuxt and Tailwind CSS</p>
-            </aside>
-            <nav>
-                <h6 class="footer-title">Contact Me</h6>
-                <NuxtLink to="mailto:me@chrisfromnowhere.me" target="_blank" class="link link-hover">Email (Personal)
-                </NuxtLink>
-                <NuxtLink to="mailto:work@chrisfromnowhere.me" target="_blank" class="link link-hover">Email (Work)
-                </NuxtLink>
-                <NuxtLink to="https://matrix.to/#/@chris1320:matrix.org" target="_blank" class="link link-hover">Matrix
-                </NuxtLink>
-            </nav>
-        </footer>
+        <AppFooter />
     </div>
 </template>
