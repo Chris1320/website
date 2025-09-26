8621f6783b57c480eb21a625d7bffbd5317e53ac
Chris1320 <me@chrisfromnowhere.me>
2025-09-26 11:45:42 +0800

    revert: remove tooltip on menu toggle
    This reverts commit 8f1999524953db6a2ad62f4d7c448d96662a24ea.
    
    Signed-off-by: Chris1320 <me@chrisfromnowhere.me>

--- components/AppMenu.vue
+++ components/AppMenu.vue
@@ -10,17 +10,21 @@ const toggleMenu = () => {
 <template>
     <ul class="menu bg-base-200 rounded-box fixed top-4 left-4 z-50">
         <li>
-            <label class="swap swap-rotate" @click="toggleMenu()">
-                <input v-model="isMenuCollapsed" type="checkbox">
-                <Icon class="swap-on" name="mdi:menu" size="20" @click="toggleMenu()" />
-                <Icon class="swap-off" name="mdi:close" size="20" @click="toggleMenu()" />
-            </label>
+            <button class="tooltip tooltip-right" data-tip="Toggle Menu" @click="toggleMenu()">
+                <label class="swap swap-rotate">
+                    <input v-model="isMenuCollapsed" type="checkbox" />
+                    <Icon class="swap-on" name="mdi:menu" size="20" @click="toggleMenu()" />
+                    <Icon class="swap-off" name="mdi:close" size="20" @click="toggleMenu()" />
+                </label>
+            </button>
         </li>
         <!-- FIXME: toggle animation does not completely work. -->
         <Motion
-v-if="!isMenuCollapsed" :initial="{ opacity: 0, y: -20 }"
+            v-if="!isMenuCollapsed"
+            :initial="{ opacity: 0, y: -20 }"
             :animate="{ opacity: 1, y: 0, transition: { duration: 0.3 } }"
-            :exit="{ opacity: 0, y: -20, transition: { duration: 0.2 } }">
+            :exit="{ opacity: 0, y: -20, transition: { duration: 0.2 } }"
+        >
             <li>
                 <NuxtLink class="tooltip tooltip-right" data-tip="Home" to="/">
                     <Icon name="mdi:home-outline" size="20" />
