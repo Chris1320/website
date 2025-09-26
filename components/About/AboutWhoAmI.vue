89a8e7a9aa667044f16ac92a23efc66f1a9c05c5
Chris1320 <me@chrisfromnowhere.me>
2025-09-26 13:38:59 +0800

    refactor: move "Who Am I?" section to a component
    Signed-off-by: Chris1320 <me@chrisfromnowhere.me>

---
+++ components/About/AboutWhoAmI.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+const yearStarted = new Date(2015, 0, 1);
+const currentTime = new Date();
+
+const yearsOfExperience = currentTime.getFullYear() - yearStarted.getFullYear();
+</script>
+
+<template>
+    <div class="pt-8">
+        <h2 class="text-2xl font-semibold mb-2">Who am I?</h2>
+        <p class="mb-4 text-xl">
+            Hi! I am Christopher Andrei Tayao, a student from
+            <NuxtLink href="https://baliuagu.edu.ph/" target="_blank">Baliuag University</NuxtLink> studying
+            <strong>BS Computer Science</strong>. I am aiming to be a full-stack software developer. I like doing random
+            things that are related to computers and technology.
+        </p>
+        <p class="mb-4 text-xl">
+            I started programming <b>{{ yearsOfExperience }} years ago</b>.
+        </p>
+    </div>
+</template>
--- pages/about.vue
+++ pages/about.vue
@@ -1,26 +1,7 @@
-<script setup lang="ts">
-const yearStarted = new Date(2015, 0, 1);
-const currentTime = new Date();
-
-const yearsOfExperience = currentTime.getFullYear() - yearStarted.getFullYear();
-</script>
-
 <template>
     <div class="container mx-auto p-4 min-h-screen">
         <h1 class="text-3xl font-bold mb-4">About Me</h1>
-        <!-- Who Am I?-->
-        <div class="pt-8">
-            <h2 class="text-2xl font-semibold mb-2">Who am I?</h2>
-            <p class="mb-4 text-xl">
-                Hi! I am Christopher Andrei Tayao, a student from
-                <NuxtLink href="https://baliuagu.edu.ph/" target="_blank">Baliuag University</NuxtLink> studying
-                <strong>BS Computer Science</strong>. I am aiming to be a full-stack software developer. I like doing
-                random things that are related to computers and technology.
-            </p>
-            <p class="mb-4 text-xl">
-                I started programming <b>{{ yearsOfExperience }} years ago</b>.
-            </p>
-        </div>
+        <AboutMe />
         <!-- Toolkit -->
         <div class="pt-8">
             <h2 class="text-2xl font-semibold mb-2">Toolkit</h2>
