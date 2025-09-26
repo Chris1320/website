998199239ccdda9a3a889c53058a47bcde7d3625
Chris1320 <me@chrisfromnowhere.me>
2025-09-27 06:07:58 +0800

    feat: add PDF export functionality to Curriculum Vitae page (only print for now)
    Signed-off-by: Chris1320 <me@chrisfromnowhere.me>

--- pages/my/cv.vue
+++ pages/my/cv.vue
@@ -5,6 +5,9 @@ const route = useRoute();
 const showImage = ref(route.query.image ? route.query.image === "true" : false);
 const showFooter = ref(route.query.footer ? route.query.footer === "true" : false);
 const fancyMode = ref(route.query.fancy ? route.query.fancy === "true" : true);
+// For PDF Export
+const isExporting = ref(false);
+const exportTitle = ref("Christopher Andrei Tayao - CV");
 
 definePageMeta({
     layout: "resume",
@@ -19,11 +22,25 @@ useHead({
     ],
     link: [{ rel: "icon", type: "image/ico", href: "/favicon.ico" }],
 });
+
+async function exportPdf() {
+    isExporting.value = true;
+    window.print();
+    // TODO: Implement PDF export functionality
+    isExporting.value = false;
+    return;
+}
 </script>
 
 <template>
     <div>
         <CurriculumVitae :show-image="showImage" :fancy-mode="fancyMode" />
+        <div class="fab">
+            <button class="btn btn-lg btn-circle btn-accent" @click="exportPdf">
+                <span v-if="isExporting" class="loading loading-spinner loading-lg" />
+                <span v-else><Icon name="mdi:printer" /></span>
+            </button>
+        </div>
         <div class="h-32" />
         <AppFooter v-show="showFooter" />
     </div>
