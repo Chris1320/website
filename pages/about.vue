e90a1e33455623cde57754e54a730402873ffb31
Chris1320 <me@chrisfromnowhere.me>
2025-10-01 00:52:47 +0800

    feat: responsive design for about page
    Signed-off-by: Chris1320 <me@chrisfromnowhere.me>

--- pages/about.vue
+++ pages/about.vue
@@ -14,6 +14,17 @@ function setHoveredTool(tool: Tool, category: string) {
     hoveredToolCategory.value = category;
 }
 
+function showHoveredToolInfo() {
+    console.log("Showing info for hovered tool.");
+    const dialog = document.getElementById("view_toolkit_info_modal") as HTMLDialogElement;
+    if (dialog && hoveredTool && hoveredToolCategory) {
+        console.log(`Showing info for tool: ${hoveredTool.value?.name} in category: ${hoveredToolCategory.value}`);
+        dialog.showModal();
+    } else {
+        console.log("No tool is currently selected to show info for.");
+    }
+}
+
 function clearHoveredTool() {
     console.log("No longer hovering over any tool.");
     hoveredTool.value = null;
@@ -49,56 +60,104 @@ onMounted(() => {
                 <AppError title="An error occured loading the toolkit" :error="error.message" />
             </div>
             <div v-else>
-                <!-- FIXME: mobile UI responsive design pls -->
                 <p class="mb-4 text-xl">Here are some of the tools and technologies I use:</p>
-                <div class="flex flex-row gap-4 items-start">
-                    <div class="max-w-3/5">
-                        <table class="table">
+                <!-- For desktop -->
+                <div class="hidden lg:block">
+                    <div class="flex flex-row gap-4 items-start">
+                        <div class="max-w-3/5">
+                            <table class="table">
+                                <tbody>
+                                    <tr v-for="toolkitCategory in toolkit" :key="toolkitCategory.name">
+                                        <td class="w-1/2">
+                                            <div class="text-lg font-semibold text-right">
+                                                <p>{{ toolkitCategory.name }}</p>
+                                                <p class="text-sm opacity-70">{{ toolkitCategory.description }}</p>
+                                            </div>
+                                        </td>
+                                        <td class="w-1/2">
+                                            <div v-if="isMounted" class="grid grid-cols-5 gap-4">
+                                                <motion.div
+                                                    v-for="tool in toolkitCategory.tools"
+                                                    :key="tool.name"
+                                                    :initial="{ opacity: 0, y: 20 }"
+                                                    :animate="{ opacity: 1, y: 0 }"
+                                                    :while-hover="{ scale: 1.5 }"
+                                                    @hover-start="setHoveredTool(tool, toolkitCategory.name)"
+                                                    @hover-end="clearHoveredTool()"
+                                                >
+                                                    <Icon :name="tool.icon" size="48" />
+                                                </motion.div>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="flex flex-col items-center max-w-1/4">
+                            <Icon :name="hoveredTool ? hoveredTool.icon : defaultIcon" size="256" />
+                            <h3 class="text-xl font-semibold mt-4">
+                                {{ hoveredTool ? hoveredTool.name : "Select a tool" }}
+                            </h3>
+                            <div v-if="hoveredToolCategory" class="badge badge-soft badge-accent mt-2">
+                                {{ hoveredToolCategory }}
+                            </div>
+                            <p class="text-center mt-2 text-sm opacity-70">
+                                {{
+                                    hoveredTool
+                                        ? hoveredTool.description
+                                        : "Hover over a tool to see its name and description."
+                                }}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <!-- For mobile -->
+                <div class="lg:hidden">
+                    <div v-for="toolkitCategory in toolkit" :key="toolkitCategory.name">
+                        <div class="text-lg font-semibold">
+                            <p>{{ toolkitCategory.name }}</p>
+                            <p class="text-sm opacity-70">{{ toolkitCategory.description }}</p>
+                        </div>
+                        <table class="table pt-10 max-w-sm">
                             <tbody>
-                                <tr v-for="toolkitCategory in toolkit" :key="toolkitCategory.name">
-                                    <td class="w-1/2">
-                                        <div class="text-lg font-semibold text-right">
-                                            <p>{{ toolkitCategory.name }}</p>
-                                            <p class="text-sm opacity-70">{{ toolkitCategory.description }}</p>
-                                        </div>
-                                    </td>
-                                    <td class="w-1/2">
-                                        <div v-if="isMounted" class="grid grid-cols-5 gap-4">
-                                            <motion.div
-                                                v-for="tool in toolkitCategory.tools"
-                                                :key="tool.name"
-                                                :initial="{ opacity: 0, y: 20 }"
-                                                :animate="{ opacity: 1, y: 0 }"
-                                                :while-hover="{ scale: 1.5 }"
-                                                @hover-start="setHoveredTool(tool, toolkitCategory.name)"
-                                                @hover-end="clearHoveredTool()"
-                                            >
-                                                <Icon :name="tool.icon" size="48" />
-                                            </motion.div>
-                                        </div>
-                                    </td>
-                                </tr>
+                                <td class="w-1/2">
+                                    <div v-if="isMounted" class="grid grid-cols-5 gap-4">
+                                        <motion.div
+                                            v-for="tool in toolkitCategory.tools"
+                                            :key="tool.name"
+                                            :initial="{ opacity: 0, y: 20 }"
+                                            :animate="{ opacity: 1, y: 0 }"
+                                            :while-hover="{ scale: 1.5 }"
+                                            @hover-start="setHoveredTool(tool, toolkitCategory.name)"
+                                            @click="showHoveredToolInfo()"
+                                        >
+                                            <Icon :name="tool.icon" size="48" />
+                                        </motion.div>
+                                    </div>
+                                </td>
                             </tbody>
                         </table>
-                    </div>
-                    <div class="flex flex-col items-center max-w-1/4">
-                        <Icon :name="hoveredTool ? hoveredTool.icon : defaultIcon" size="256" />
-                        <h3 class="text-xl font-semibold mt-4">
-                            {{ hoveredTool ? hoveredTool.name : "Select a tool" }}
-                        </h3>
-                        <div v-if="hoveredToolCategory" class="badge badge-soft badge-accent mt-2">
-                            {{ hoveredToolCategory }}
-                        </div>
-                        <p class="text-center mt-2 text-sm opacity-70">
-                            {{
-                                hoveredTool
-                                    ? hoveredTool.description
-                                    : "Hover over a tool to see its name and description."
-                            }}
-                        </p>
                     </div>
                 </div>
             </div>
         </div>
+        <dialog id="view_toolkit_info_modal" class="modal" @close="clearHoveredTool()">
+            <div class="modal-box modal-color-override">
+                <div class="flex flex-col items-center pb-5">
+                    <Icon :name="hoveredTool?.icon || defaultIcon" size="128" class="my-4 text-center" />
+                    <div class="badge badge-soft badge-accent text-center">
+                        {{ hoveredToolCategory || "No category" }}
+                    </div>
+                    <h3 class="text-xl font-bold text-center">{{ hoveredTool?.name || "No tool selected" }}</h3>
+                </div>
+                <p>{{ hoveredTool?.description || "Please select a tool first" }}</p>
+                <div class="modal-action">
+                    <form method="dialog">
+                        <!-- if there is a button in form, it will close the modal -->
+                        <button class="btn">Close</button>
+                    </form>
+                </div>
+            </div>
+        </dialog>
     </div>
 </template>
