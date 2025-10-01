88d6b16d80ca6da1d38e4fb2c6f52e631bb58e23
Chris1320 <me@chrisfromnowhere.me>
2025-10-02 01:23:19 +0800

    fix: add `object-contain` class to images in carousel for better display
    Signed-off-by: Chris1320 <me@chrisfromnowhere.me>

--- components/Projects/Mockup/ProjectsMockupContent.vue
+++ components/Projects/Mockup/ProjectsMockupContent.vue
@@ -22,7 +22,7 @@ function nextImage() {
             :key="index"
             class="carousel-item relative w-full"
         >
-            <img :src="image.url" class="w-full" :alt="image.description" />
+            <img :src="image.url" class="w-full object-contain" :alt="image.description" />
             <div
                 v-show="images.length > 1"
                 class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between"
