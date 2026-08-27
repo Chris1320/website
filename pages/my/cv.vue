d9673f883b69017a37cc643b1be7a6aa21d54905
Chris1320 <me@chrisfromnowhere.me>
2026-08-28 00:01:31 +0800

    fix: update `/my/cv` to respect theme setting
    Signed-off-by: Chris1320 <me@chrisfromnowhere.me>

--- pages/my/cv.vue
+++ pages/my/cv.vue
@@ -1,28 +1,34 @@
 <script setup lang="ts">
-const route = useRoute();
-const showFooter = ref(route.query.footer ? route.query.footer === "true" : false);
+const { theme, initTheme } = useTheme();
 
-definePageMeta({
-    layout: "resume",
+if (import.meta.client) {
+    initTheme();
+}
+
+onMounted(() => {
+    initTheme();
 });
 
 useHead({
-    title: "CV - Christopher Andrei Tayao",
+    title: "CV | Christopher Andrei Tayao",
+    htmlAttrs: {
+        "data-theme": computed(() => theme.value),
+        lang: "en",
+    },
     meta: [
         { name: "description", content: "Christopher Andrei Tayao's Curriculum Vitae" },
         { name: "viewport", content: "width=device-width, initial-scale=1" },
-        { name: "theme-color", content: "#ffffff" },
+        { name: "theme-color", content: computed(() => (theme.value === "dark" ? "#1e1e2e" : "#eff1f5")) },
     ],
     link: [{ rel: "icon", type: "image/ico", href: "/favicon.ico" }],
 });
 </script>
 
 <template>
-    <div class="flex flex-col justify-center items-center w-full max-h-lvh px-4 py-8">
+    <div :data-theme="theme" class="flex flex-col justify-center items-center w-full px-4 pt-2 pb-8">
         <iframe
             src="https://drive.google.com/file/d/1psTI9qCELxiXs6cZ3WjGX4VJMwpESRfz/preview"
             class="w-3/4 min-h-screen rounded-lg shadow-lg border"
         />
-        <AppFooter v-show="showFooter" />
     </div>
 </template>
